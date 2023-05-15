e3748911d27b668b732581793ac02408d16e44e6
kzhen <kzhen@cs.washington.edu>
2023-05-15 16:51:18 -0700

    react

---
+++ frontend/src/components/display.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './display.css'
+
+
+
+function display() {
+  return (
+    <div>
+      <h1> display page comes next</h1>
+
+    </div>
+  )
+}
+    
+  export default display;
+  
