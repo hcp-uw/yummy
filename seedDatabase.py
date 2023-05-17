18c4b31bfc02f2a2ec40726c993cdaf9daa6dfaf
whu3 <whu3@cs.washington.edu>
2023-05-16 18:25:10 -0700

    Implemented add ingredients from json to database

--- seedDatabase.py
+++ seedDatabase.py
@@ -14,9 +14,14 @@ clearCommand = "DELETE FROM public.\"Recipes\""
 seedCommand = "INSERT INTO public.\"Recipes\"(name, ingredients, prep_time, servings, cook_time, cuisine, link) VALUES"
 selectCommand = "SELECT name, ingredients, prep_time, servings, cook_time, cuisine, link FROM public.\"Recipes\";"
 
+clearIngredientCommand = "DELETE FROM public.\"Ingredients\""
+seedIngredientCommand = "INSERT INTO public.\"Ingredients\"(\"Ingredient\", \"Category\")	VALUES"
+selectIngredientCommand = "SELECT \"Ingredient\", \"Category\" FROM public.\"Ingredients\""
+
 # First clear the database entries
 cursor = conn.cursor()
 cursor.execute(clearCommand)
+cursor.execute(clearIngredientCommand)
 
 # Open json file and add database items
 f = open('recipes.json')
@@ -26,9 +31,25 @@ for i in data:
 
 f.close()
 
+ingredients = []
+with open('Food.json', 'r', encoding='utf8') as f:
+    for ingredient in f:
+        added_ingredient = json.loads(ingredient)
+        added_ingredient['name'] = added_ingredient['name'].replace("'", "")  # an ingredient had a ' and was causing issues
+        ingredients.append(added_ingredient)
+
+for i in ingredients:
+    cursor.execute(f"{seedIngredientCommand} (\'{i['name']}\', \'{i['food_subgroup']}\')")
+
+f.close()
+
 # Print database entries
 cursor = conn.cursor()
 cursor.execute(selectCommand)
+result = cursor.fetchall()
+print(result)
+
+cursor.execute(selectIngredientCommand)
 result = cursor.fetchall()
 print(result)
 
